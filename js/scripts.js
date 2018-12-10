9db052126b2157c35eedc05f953f1871d5833269
dchulax <tobiasorao@gmail.com>
2018-12-10 15:39:50 +0300

    Add getCheckedValue

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,8 @@
 var correctAnswer = ["B","B","C","A"],
     total = correctAswer.length;
+function getCheckedValue(radioName){
+  var radios =document.getElementByName(radioName);
+  for (var i=0; i<radios.length; i++)
+  if(radios[i].checked)
+  return radios[i].value;
+}
